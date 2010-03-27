5245c67fcc9b2b685de630f51f746bfbaad2b89f
Adam McCrea <adam@adamlogic.com>
2010-03-27 15:13:06 -0400

    Remove zsh prompt "unpushed" and "dirty" icons
    - These were really slowing down the prompt in large repos (like edge rails)

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,37 +1,3 @@
-function pushed() {
-  if [ $@ ]; then
-    git cherry -v origin/$@
-  else
-    git cherry -v origin/$(git_branch_name)
-  fi
-}
-
-function git_branch_name() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-}
-
-function is_working_directory_dirty() {
- if current_git_status=$(git status 2> /dev/null | grep --regex="deleted\|modified\|Untracked" 2> /dev/null); then
-   echo "⚡"
- else
-   echo ''
- fi
-}
-
-function unpushed() {
-  git cherry -v origin/$(git_branch_name) 2>/dev/null
-}
-
-function need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo "%{\e[0;33m%} ↑%{\e[0m%}"
-  fi
-}
-
-
 function gitdays {
   git log --author=Pairing --reverse --since="$@ days ago" --pretty="format:%n%Cgreen%cd%n%n%s%n%b%n---------------------------------------------" 
 }
@@ -48,16 +14,6 @@ function pbhaml {
 
 function md {
   markdown.pl $@ > /tmp/generated_by_markdown.html; open /tmp/generated_by_markdown.html
-}
-
-function set_prompt() {
-  export PS1='%2/ ~ '
-
-  branch_name=$(git_branch_name)
-  if [ -n "$branch_name" ]; then
-    export PS1='%1~%{$reset_color$bold_color$fg[green]%}%{$reset_color$fg[green]%} ($branch_name)%{$reset_color%} ~ '
-    export RPS1="%{$fg[magenta]%}$(is_working_directory_dirty)%{$fg[yellow]%}$(need_push)%{$reset_color%}"
-  fi
 }
 
 function railstags() {
--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -8,6 +8,19 @@ set_term_tab() {
   echo -ne "\e]1;$PWD:h:t/$PWD:t\a" 
 }
 
+function set_prompt() {
+  export PS1='%2/ ~ '
+
+  branch_name=$(git_branch_name)
+  if [ -n "$branch_name" ]; then
+    export PS1='%1~%{$reset_color$bold_color$fg[green]%}%{$reset_color$fg[green]%} ($branch_name)%{$reset_color%} ~ '
+  fi
+}
+
+function git_branch_name() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
 precmd() { 
   set_term_title
   set_term_tab
