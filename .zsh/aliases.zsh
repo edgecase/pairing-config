dfc3aff28e6913f388fb34d3c9cd6c07805806bc
Jerry Nummi <jerry@edgecase.com>
2010-05-15 18:17:56 -0400

    ZSH aliases cleanup.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,40 +1,50 @@
+alias ajaxrdoc="rdoc --fmt ajax --exclude '.*generator.*' --exclude '.*test.*' --exclude '.*spec.*'"
 alias c='clear'
 alias cpwd="pwd | ruby -pe'\$_.chomp!' | pbcopy"
-alias gitrm='git ls-files --deleted | xargs git rm'
 alias grep='grep --color --line-number'
 alias gvim='mvim -p'
-alias la='ls -lahF'
-alias log='tail -f log/development.log'
-alias mv='mv -i'
+alias ll='ls -lF' # long format listing; add / to end of directories
+alias la='ls -aF' # include dot files in listing; add / to end of directories
+alias mv='mv -i' # prompt before moving a file that would overwrite an existing file
+
+
+alias icons='cd ~/Pictures/icons; open -a Preview **/*.png **/*.tiff  **/*.jpg'
+alias preview='open -a Preview'
+
+
+# Databses
+
 alias mysql='/opt/local/bin/mysql5 -u root --socket=/tmp/mysql.sock'
 alias mysqladmin='/opt/local/bin/mysqladmin5 -u root --socket=/tmp/mysql.sock'
 alias mysql_config='/opt/local/bin/mysql_config5'
 alias postgres_start='pg_ctl -D ~/.pgdata -l ~/.pgdata/psql.log start'
 alias postgres_stop='pg_ctl -D ~/.pgdata stop'
-alias push='git push'
-alias pushed\?='pushed'
-alias restart='touch tmp/restart.txt'
-alias ajaxrdoc="rdoc --fmt ajax --exclude '.*generator.*' --exclude '.*test.*' --exclude '.*spec.*'"
+
+# Rails
+
 alias devlog='tail -200 -f log/development.log'
-alias testlog='tail -200 -f log/test.log'
-alias gvim='mvim -p'
-alias gitrm='git ls-files --deleted | xargs git rm'
-alias icons='cd ~/Pictures/icons; open -a Preview **/*.png **/*.tiff  **/*.jpg'
-alias preview='open -a Preview'
-alias rebase='git fetch; git rebase origin/master'
+alias log='tail -f log/development.log'
 alias restart='touch tmp/restart.txt'
+alias rc='./script/rails console'
+alias rg='./script/rails generate'
+alias rs='./script/rails server'
 alias sc='./script/console'
 alias sg='./script/generate'
 alias sp='./script/spec -cfs'
 alias ss='./script/server'
-alias rc='./script/rails console'
-alias rg='./script/rails generate'
-alias rs='./script/rails server'
-alias st='git status'
+alias ssd='./script/server --debugger'
+alias testlog='tail -200 -f log/test.log'
+
+# Git
+
+alias gitrm='git ls-files --deleted | xargs git rm'
 alias gitx='gitx --all'
 alias gpci='git-pair commit'
 alias gp='git pair'
+alias push='git push'
+alias rebase='git fetch; git rebase origin/master'
+alias st='git status'
+
+# Project shortcuts
+
 alias bwm='cd ~/projects/buywithme'
-alias ssd='./script/server --debugger'
-alias ll='ls -l'
-alias la='ls -a'
