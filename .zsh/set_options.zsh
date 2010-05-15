1244b610c2535831177e3fb3b3be14fa2e9b7520
Jerry Nummi <jerry@edgecase.com>
2010-05-15 17:42:34 -0400

    ZSH options cleanup.

--- .zsh/set_options.zsh
+++ .zsh/set_options.zsh
@@ -1,10 +1,10 @@
-setopt APPEND_HISTORY 
-setopt INC_APPEND_HISTORY 
-setopt HIST_IGNORE_DUPS
-setopt HIST_FIND_NO_DUPS
-setopt complete_in_word         # Not just at the end
 setopt always_to_end            # When complete from middle, move cursor
+setopt append_history           # Append to history file, not overwrite it
+setopt auto_cd                  # If a command is not in the hash table, and there exists an executable directory by that name, perform the cd command to that directory.
+setopt complete_in_word         # Always auto complete, not just at the end
+setopt extendedglob             # Extra filename generation features
+setopt hist_ignore_dups         # Don't enter command in history if it is duplicate of previous command
+setopt hist_find_no_dups        # Don't display duplicates when searching commands
+setopt inc_append_history       # Append history item as soon as command is executed, rather than waiting until the shell is killed
 setopt nohup										# In general, we don't kill background jobs upon logging out
-setopt prompt_subst
-setopt auto_cd
-setopt extendedglob
+setopt prompt_subst             # parameter expansion, command substitution and arithmetic expansion is performed in prompts
--- .zshrc
+++ .zshrc
@@ -1,16 +1,14 @@
 autoload -U compinit && compinit
 autoload colors && colors
 
-source ~/.zsh/set_options.zsh
+source ~/.zsh/aliases.zsh
+source ~/.zsh/bindkeys.zsh
+source ~/.zsh/completion.zsh
+source ~/.zsh/completion_rake.zsh
 source ~/.zsh/exports.zsh
 source ~/.zsh/functions.zsh
 source ~/.zsh/history.zsh
-source ~/.zsh/completion.zsh
-source ~/.zsh/completion_rake.zsh
-source ~/.zsh/aliases.zsh
-source ~/.zsh/bindkeys.zsh
 source ~/.zsh/prompt.zsh
+source ~/.zsh/set_options.zsh
 
 if [[ -s ~/.rvm/scripts/rvm ]] ; then source ~/.rvm/scripts/rvm ; fi
-
-
