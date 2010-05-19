6e3e3f3a7cf23471e44ed80b970ded9cda18f119
Felix Flores + Marc Peabody <pairing+ff+mp@edgecase.com>
2010-05-19 13:43:15 -0400

    added author initials to prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -12,13 +12,18 @@ function set_prompt() {
   export PS1='%2/ ~ '
 
   branch_name=$(git_branch_name)
+  author_name=$(git_author_name)
   if [ -n "$branch_name" ]; then
-    export PS1='%1~%{$reset_color$bold_color$fg[green]%}%{$reset_color$fg[green]%} ($branch_name)%{$reset_color%} ~ '
+    export PS1='%1~%{$reset_color$bold_color$fg[green]%}%{$reset_color$fg[green]%} ($author_name: $branch_name)%{$reset_color%} ~ '
   fi
 }
 
 function git_branch_name() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+function git_author_name() {
+  git config --get user.name | sed 's/\([a-z ]*\)//g' | tr '[A-Z]' '[a-z]'
 }
 
 precmd() { 
