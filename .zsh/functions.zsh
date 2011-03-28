cb05b48a2f2f521238e570bc6ad7b71f49206d17
Ken Barker <ken@theedgecase.com>
2011-03-27 22:23:56 -0400

    More functions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -47,6 +47,32 @@ function internet\? {
   (ping -c 3 -t 3 google.com >/dev/null 2>&1 && echo 'yep') || echo 'nope'
 }
 
+function git_bd { # git local branch deletion
+  # deletes any pivotally numbered feature branches
+  # branches without a remote are kept
+  # branches with unpushed changes are kept
+  git checkout production
+
+  for bra in `git branch | awk '/^([0-9].*)/ {print $1}'`
+  do
+    set -A o `git branch -r | grep $bra`
+    if [ "$o" != "" ]
+    then
+      set -A diff "`git cherry -v $o $bra`"
+      if [ "$diff" != "" ]
+      then
+        echo $bra has unpushed changes. not deleting
+      else
+        git branch -D $bra
+      fi
+    else
+      echo $bra did not have an origin of the same name
+    fi
+  done
+
+  git branch
+}
+
 function git_bd_merged { # git local branch deletion
   # deletes any local feature branches
   # branches with unmerged commits to production are kept
@@ -79,6 +105,41 @@ function git_bd_merged { # git local branch deletion
 
   git branch
 }
+
+function git_bd_merged_remote { # git local branch deletion
+  # deletes any local feature branches
+  # branches with unmerged commits to production are kept
+  git checkout production
+  skip=('*' production master staging acceptance)
+  # for bra in `git branch | awk '/(.*)/ {print $1}'`
+  for bra in `git branch | awk '/([A-z0-9_-].*)/ {print $1}'`
+  do
+    set -A skipit "false"
+    for sk in $skip
+    do
+      if [ "$bra" = "$sk" ]
+      then
+        set -A skipit "true"
+      fi
+    done
+    if [ "$skipit" = "true" ]
+    then
+      echo skipping $bra
+      continue
+    fi
+    set -A diff "`git cherry -v production $bra`"
+    if [ "$diff" != "" ]
+    then
+      echo $bra has unmerged commits. not deleting
+    else
+      git branch -D $bra
+      git push origin :$bra
+    fi
+  done
+
+  git branch
+}
+alias gbdmr='git_bd_merged_remote'
 
 function gdt {
   git-delete-tags $@
