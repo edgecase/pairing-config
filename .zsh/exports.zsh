11019757054661e967353221f9fec13d644676e7
Adam McCrea <adam@edgecase.com>
2011-04-10 17:11:18 -0400

    Remove Dropbox bin directory
    - Put scripts in the bin directory in this repo instead

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,4 +1,4 @@
-export PATH=$PATH:/usr/local/bin:/usr/local/sbin:~/Dropbox/pairing/bin:~/bin
+export PATH=$PATH:/usr/local/bin:/usr/local/sbin:~/bin
 
 export CDPATH=:~/projects
 export RUBYOPT='rubygems -Itest'
