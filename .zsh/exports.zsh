f9c9af7c7157460786587bb18bae68f48f566c67
swalke16 <swalke16@gmail.com>
2011-04-21 13:44:08 -0400

    make sure terminal vim is using the mvim version of vim

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -2,7 +2,7 @@ export PATH=$PATH:/usr/local/bin:/usr/local/sbin:~/bin
 
 export CDPATH=:~/projects
 export RUBYOPT='rubygems -Itest'
-export GIT_EDITOR="vim"
+export GIT_EDITOR="mvim -v" # use mvim version of vim as git editor
 export EDITOR="mvim"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 if [[ $TERM != 'dumb' ]]; then
