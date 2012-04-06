dc7679861e9d51cb078363fe7cb2a8ddae31fac2
swalke16 <swalke16@gmail.com>
2012-04-06 10:13:21 -0400

    added thin and postgres recipes

---
+++ rails-bootstrap/recipes/optional/postgres.rb
@@ -0,0 +1,40 @@
+gem 'pg'
+
+host = ask?("What is your datbase host? [localhost]")
+host = "localhost" if host.blank?
+database = ask?("What is the name of the database? [#{app_name}]")
+database = app_name if database.blank?
+
+after_bundler do
+  remove_file "config/database.yml"
+  remove_file "db/development.sqlite3"
+  remove_file "db/test.sqlite3"
+
+  create_file "config/database.yml", <<-CFG
+common: &common
+  host: #{host}
+  adapter: postgresql
+
+development:
+  <<: *common
+  database: #{database}_dev
+
+test: &test
+  <<: *common
+  database: #{database}_test
+
+cucumber:
+  <<: *test
+
+production:
+  <<: *common
+  database: #{database}_prod
+CFG
+
+rake "db:create", :env => 'development'
+rake "db:migrate", :env => 'development'
+rake "db:test:prepare"
+
+end
+
+
