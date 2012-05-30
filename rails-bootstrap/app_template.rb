894351e33796416e1cf056583845d4466b234fa3
Alan Gardner <urfolomeus@gmail.com>
2012-05-30 09:24:50 +0100

    Converting the existing app template to use the newer style

--- rails-bootstrap/app_template.rb
+++ rails-bootstrap/app_template.rb
@@ -1,7 +1,26 @@
 require File.join(File.dirname(__FILE__), 'core_extensions.rb')
 
-required = recipes_in('required')
-optional = recipes_in('optional')
+required = [
+  'application',
+  'cleanup',
+  'cucumber',
+  'factory_girl',
+  'gem',
+  'gitignore',
+  'haml',
+  'rspec',
+  'simple_form'
+].map{|r| File.join(RECIPES_ROOT, 'required', "#{r}.rb")}
+
+optional = [
+  'compass',
+  'devise',
+  'disable_coffeescript',
+  'postgres',
+  'pry',
+  'thin',
+  'vcr'
+].map{|r| File.join(RECIPES_ROOT, 'optional', "#{r}.rb")}
 
 bootstrap(required, optional)
 
